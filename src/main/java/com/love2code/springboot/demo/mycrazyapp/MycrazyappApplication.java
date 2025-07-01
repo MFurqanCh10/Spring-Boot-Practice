8a5d0ecafb724556db5f9ecc13df24ffb5b05ca0
Muhammad Furqan <itxmfurqan@gmail.com>
2025-07-01 18:25:37 +0500

    Spring Boot 3 Quick Start

---
+++ src/main/java/com/love2code/springboot/demo/mycrazyapp/MycrazyappApplication.java
@@ -0,0 +1,13 @@
+package com.love2code.springboot.demo.mycrazyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MycrazyappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MycrazyappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/love2code/springboot/demo/mycrazyapp/MycrazyappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.love2code.springboot.demo.mycrazyapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MycrazyappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
